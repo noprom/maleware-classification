82857814161991c73546a07b1b393c77a9771d3b
noprom <tyee.noprom@qq.com>
2016-09-09 17:24:26 +0800

    add run.sh

---
+++ bin/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# 判断参数个数
+if [ $# != 1 ]; then
+    echo "Usage: $0 the-file-you-want-to-submit.jar";
+    exit 0;
+fi
+
+# 判断提交的文件是否存在
+FILENAME=$1
+if [ ! -f "$FILENAME" ]; then
+    echo "$FILENAME does not exist.";
+    exit 0;
+fi
+cur_dir=$(cd "$(dirname "$0")"; pwd)
+FILENAME="$cur_dir/$FILENAME"
+# 提取主机名
+HOSTNAME=`hostname`
+echo "Running spark on : $HOSTNAME";
+
+# 提交Spark Job
+SUBMIT_URL=spark://$HOSTNAME:7077
+$SPARK_HOME/bin/spark-submit \
+  --master $SUBMIT_URL \
+  --executor-memory 6G \
+  --total-executor-cores 4 \
+  $FILENAME
