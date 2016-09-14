78ef8b2dba9dcbaaa29df916483f3c3a4b49a77b
noprom <tyee.noprom@qq.com>
2016-09-14 10:28:07 +0800

    add CountVectorizerExample

--- spark-examples/src/main/scala/org/apache/spark/feature/extract/TfIdfExample.scala
+++ spark-examples/src/main/scala/org/apache/spark/feature/extract/TfIdfExample.scala
@@ -16,41 +16,42 @@
  */
 
 // scalastyle:off println
-package org.apache.spark.ets
+package org.apache.spark.feature.extract
 
 // $example on$
-import org.apache.spark.ml.feature.Word2Vec
-import org.apache.spark.ml.linalg.Vector
-import org.apache.spark.sql.Row
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
 // $example off$
 import org.apache.spark.sql.SparkSession
 
-object Word2VecExample {
+object TfIdfExample {
+
   def main(args: Array[String]) {
     val spark = SparkSession
       .builder
-      .appName("Word2Vec example")
+      .appName("TfIdfExample")
       .getOrCreate()
 
     // $example on$
-    // Input data: Each row is a bag of words from a sentence or document.
-    val documentDF = spark.createDataFrame(Seq(
-      "Hi I heard about Spark".split(" "),
-      "I wish Java could use case classes".split(" "),
-      "Logistic regression models are neat".split(" ")
-    ).map(Tuple1.apply)).toDF("text")
+    val sentenceData = spark.createDataFrame(Seq(
+      (0.0, "Hi I heard about Spark"),
+      (0.0, "I wish Java could use case classes"),
+      (1.0, "Logistic regression models are neat")
+    )).toDF("label", "sentence")
 
-    // Learn a mapping from words to Vectors.
-    val word2Vec = new Word2Vec()
-      .setInputCol("text")
-      .setOutputCol("result")
-      .setVectorSize(3)
-      .setMinCount(0)
-    val model = word2Vec.fit(documentDF)
+    val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
+    val wordsData = tokenizer.transform(sentenceData)
 
-    val result = model.transform(documentDF)
-    result.collect().foreach { case Row(text: Seq[_], features: Vector) =>
-      println(s"Text: [${text.mkString(", ")}] => \nVector: $features\n") }
+    val hashingTF = new HashingTF()
+      .setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(20)
+
+    val featurizedData = hashingTF.transform(wordsData)
+    // alternatively, CountVectorizer can also be used to get term frequency vectors
+
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+    val idfModel = idf.fit(featurizedData)
+
+    val rescaledData = idfModel.transform(featurizedData)
+    rescaledData.select("label", "features").show()
     // $example off$
 
     spark.stop()
--- spark-examples/src/main/scala/org/apache/spark/feature/extract/Word2VecExample.scala
+++ spark-examples/src/main/scala/org/apache/spark/feature/extract/Word2VecExample.scala
@@ -16,42 +16,41 @@
  */
 
 // scalastyle:off println
-package org.apache.spark.ets
+package org.apache.spark.feature.extract
 
 // $example on$
-import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
+import org.apache.spark.ml.feature.Word2Vec
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.sql.Row
 // $example off$
 import org.apache.spark.sql.SparkSession
 
-object TfIdfExample {
-
+object Word2VecExample {
   def main(args: Array[String]) {
     val spark = SparkSession
       .builder
-      .appName("TfIdfExample")
+      .appName("Word2Vec example")
       .getOrCreate()
 
     // $example on$
-    val sentenceData = spark.createDataFrame(Seq(
-      (0.0, "Hi I heard about Spark"),
-      (0.0, "I wish Java could use case classes"),
-      (1.0, "Logistic regression models are neat")
-    )).toDF("label", "sentence")
+    // Input data: Each row is a bag of words from a sentence or document.
+    val documentDF = spark.createDataFrame(Seq(
+      "Hi I heard about Spark".split(" "),
+      "I wish Java could use case classes".split(" "),
+      "Logistic regression models are neat".split(" ")
+    ).map(Tuple1.apply)).toDF("text")
 
-    val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
-    val wordsData = tokenizer.transform(sentenceData)
+    // Learn a mapping from words to Vectors.
+    val word2Vec = new Word2Vec()
+      .setInputCol("text")
+      .setOutputCol("result")
+      .setVectorSize(3)
+      .setMinCount(0)
+    val model = word2Vec.fit(documentDF)
 
-    val hashingTF = new HashingTF()
-      .setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(20)
-
-    val featurizedData = hashingTF.transform(wordsData)
-    // alternatively, CountVectorizer can also be used to get term frequency vectors
-
-    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-    val idfModel = idf.fit(featurizedData)
-
-    val rescaledData = idfModel.transform(featurizedData)
-    rescaledData.select("label", "features").show()
+    val result = model.transform(documentDF)
+    result.collect().foreach { case Row(text: Seq[_], features: Vector) =>
+      println(s"Text: [${text.mkString(", ")}] => \nVector: $features\n") }
     // $example off$
 
     spark.stop()
