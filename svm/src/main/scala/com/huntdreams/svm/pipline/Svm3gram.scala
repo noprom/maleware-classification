34ccd67efa8506cc05d17db07fef0ff1e3c3a325
peterhelin <helin199210@outlook.com>
2016-09-14 22:18:36 +0800

    ch

--- svm/src/main/scala/com/huntdreams/svm/pipline/Svm3gram.scala
+++ svm/src/main/scala/com/huntdreams/svm/pipline/Svm3gram.scala
@@ -1,3 +1,4 @@
+import scala.util.matching.Regex
 /**
   *
   * Author: helin <1006604973@qq.com>.
@@ -5,5 +6,24 @@
   */
 
 object Svm3gram{
+  def main(args: Array[String]): Unit = {
+    val final
+    val spark = SparkSession
+      .builder
+      .appName("Svm3gram")
+      .getOrCreate()
+
+    val trainFile = spark.read.("/Users/helin/Documents/Pro/Spark/Pro/maleware-classification/local_data/subtrain/B7O90Mfiz5EPpJyTdXFb.asm")
+    val Decimal = """\s([a-fA-F0-9]{2}\s)+\s*([a-z]+)""".r
+
+
+
+
+    val input = """.text:00409A44 EB 0A							       jmp     short loc_409A50""""
+    for(s<-Decimal findAllIn input)
+      s.getClass
+  }
 
 }
+
+
